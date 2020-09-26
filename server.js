663346e32b8b1bbdeb0fea2486bbb6712806b5e3
Gerardo Vega <gerdanvegros@gmail.com>
2020-09-26 13:47:16 -0600

    get parameters values

---
+++ server.js
@@ -0,0 +1,15 @@
+// http: //localhost:8080/?num1=2&num2=2
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var q = url.parse(req.url, true).query;
+    res.end(addNumer(q.num1, q.num2));
+}).listen(8080);
+
+
+function addNumer(a, b) {
+    return a + b
+}
