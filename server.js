3e66cda9ae15d9c20c46c130456733f7891482b9
Gerardo Vega <gerdanvegros@gmail.com>
2020-09-28 22:35:45 -0600

    arreglando bug que concatenaba en vez de sumar

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // http: //localhost:8080/?num1=2&num2=2
 
-var http = require('http');
-var url = require('url');
+var http = require('http')
+var url = require('url')
 
 http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var q = url.parse(req.url, true).query;
-    res.end(require('./operation').addNumer(q.num1, q.num2));
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    var q = url.parse(req.url, true).query
+    var a = Number(q.num1)
+    var b = Number(q.num2)
+    var result = require('./operation').addNumer(a, b)
+    res.end(result.toString())
 }).listen(8080);
